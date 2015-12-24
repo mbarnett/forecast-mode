d67b2227e31d252dcaddcb4c409c05c031abc195
Matt Barnett <matt@sixtyodd.com>
2015-12-24 13:23:59 -0700

    progress

--- forecast-mode.el
+++ forecast-mode.el
@@ -1,12 +1,19 @@
 (require 'web)
 
+(defgroup weatherline nil
+  "Options for forecast mode"
+  :prefix "forecast-")
+
+
 (defconst forecast-geonames-service-endpoint "http://api.geonames.org/searchJSON?q=%s&maxRows=1&username=demo"
   "location of the geonames endpoint")
 
 
 (defvar forecast-location-name "")
 
-(defvar forecast-coordinates nil)
+(defcustom forecast-coordinates nil
+  ""
+  :group 'forecast)
 
 (defun forecast-geonames-service (for-location)
     (format forecast-geonames-service-endpoint for-location))
@@ -15,7 +22,7 @@
   (let* ((geodata (aref (cdr (assoc 'geonames location-data)) 0))
          (lat (cdr (assoc 'lat geodata)))
          (lng (cdr (assoc 'lng geodata))))
-    (setq frtemp (lat . lng))))
+    (customize-save-variable 'forecast-test (lat . lng))))
 
 (defun forecast-get-location-coordinates ()
     (web-http-get (lambda (conn-proc header data)
